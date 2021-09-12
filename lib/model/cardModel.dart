c1a15fc6f736b171c65260e0681f22556b16fcf8
Sumana Basu <sumana.basu2001@gmail.com>
2021-09-12 22:17:32 +0530

    Feat: Card details added

--- lib/model/cardModel.dart
+++ lib/model/cardModel.dart
@@ -9,20 +9,164 @@ class CardModel {
 
 List<CardModel> cardContent = [
   CardModel(
-      title: 'General Stuff',
-      content: 'something',
+      title: 'Hello',
+      content: 'Hello',
       image:
           'https://cdn.dribbble.com/users/623359/screenshots/13670617/media/b020cb592153c999817dca341de2d782.png',
       category: "General"),
   CardModel(
-      title: 'Pizza',
-      content: 'I want to eat some pizza',
+      title: 'Yes',
+      content: 'Yes',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670617/media/b020cb592153c999817dca341de2d782.png',
+      category: "General"),
+  CardModel(
+      title: 'No',
+      content: 'No thank you',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670617/media/b020cb592153c999817dca341de2d782.png',
+      category: "General"),
+  CardModel(
+      title: 'Help',
+      content: 'I need help',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670617/media/b020cb592153c999817dca341de2d782.png',
+      category: "General"),
+  CardModel(
+      title: 'Again',
+      content: 'Could you please repeat again?',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670617/media/b020cb592153c999817dca341de2d782.png',
+      category: "General"),
+  CardModel(
+      title: 'Thank you',
+      content: 'Thank you',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670617/media/b020cb592153c999817dca341de2d782.png',
+      category: "General"),
+  CardModel(
+      title: 'Water',
+      content: 'Please give me some water',
       image:
           'https://cdn.dribbble.com/users/623359/screenshots/13670602/media/0f15bc59541b4176455d502adc1f2cb2.png',
       category: "Food"),
   CardModel(
-      title: 'Title Three',
-      content: 'Content Three',
+      title: 'Rice',
+      content: 'I would like some rice',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670602/media/0f15bc59541b4176455d502adc1f2cb2.png',
+      category: "Food"),
+  CardModel(
+      title: 'Bread',
+      content: 'I would like some bread',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670602/media/0f15bc59541b4176455d502adc1f2cb2.png',
+      category: "Food"),
+  CardModel(
+      title: 'Chicken',
+      content: 'I would like some chicken',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670602/media/0f15bc59541b4176455d502adc1f2cb2.png',
+      category: "Food"),
+  CardModel(
+      title: 'Salad',
+      content: 'I would like some salad',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670602/media/0f15bc59541b4176455d502adc1f2cb2.png',
+      category: "Food"),
+  CardModel(
+      title: 'Ice cream',
+      content: 'I would like some ice cream',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670602/media/0f15bc59541b4176455d502adc1f2cb2.png',
+      category: "Food"),
+  CardModel(
+      title: 'Fruits',
+      content: 'I would like some fruits',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670602/media/0f15bc59541b4176455d502adc1f2cb2.png',
+      category: "Food"),
+  CardModel(
+      title: 'Cold drink',
+      content: 'I would like some cold drinks',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/13670602/media/0f15bc59541b4176455d502adc1f2cb2.png',
+      category: "Food"),
+  CardModel(
+      title: 'Wash hands',
+      content: 'I need to wash my hands',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Movements"),
+  CardModel(
+      title: 'Shower',
+      content: 'I need to take a shower',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Movements"),
+  CardModel(
+      title: 'Toilet',
+      content: 'I need to use the toilet',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Movements"),
+  CardModel(
+      title: 'Clothes',
+      content: 'I need to change my clothes',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Movements"),
+  CardModel(
+      title: 'Get up',
+      content: 'I need to get up',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Movements"),
+  CardModel(
+      title: 'Lie down',
+      content: 'I need to lie down',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Movements"),
+  CardModel(
+      title: 'Extra Pillow',
+      content: 'Please may I have another pillow',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Movements"),
+  CardModel(
+      title: 'Position',
+      content: 'Please change my position',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Movements"),
+  CardModel(
+      title: 'Headache',
+      content: 'I have a headache',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Health"),
+  CardModel(
+      title: 'Sore throat',
+      content: 'I have a sore throat',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Health"),
+  CardModel(
+      title: 'Back itch',
+      content: 'My back itches',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Health"),
+  CardModel(
+      title: 'Good',
+      content: 'I feel good',
+      image:
+          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+      category: "Health"),
+  CardModel(
+      title: 'Vomit',
+      content: 'I am going to vomit',
       image:
           'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
       category: "Health"),
