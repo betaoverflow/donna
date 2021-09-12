5feeae197e81256449d0ef685f4a5ccd2c8b282a
Aabhas <aabhassao0@gmail.com>
2021-09-12 20:26:59 +0530

    Feat: Filter cards by category
    Signed-off-by: Aabhas <aabhassao0@gmail.com>

--- lib/model/cardModel.dart
+++ lib/model/cardModel.dart
@@ -9,14 +9,14 @@ class CardModel {
 
 List<CardModel> cardContent = [
   CardModel(
-      title: 'Title One',
-      content: 'Content One',
+      title: 'General Stuff',
+      content: 'something',
       image:
           'https://cdn.dribbble.com/users/623359/screenshots/13670617/media/b020cb592153c999817dca341de2d782.png',
       category: "General"),
   CardModel(
-      title: 'Title Two',
-      content: 'Content Two',
+      title: 'Pizza',
+      content: 'I want to eat some pizza',
       image:
           'https://cdn.dribbble.com/users/623359/screenshots/13670602/media/0f15bc59541b4176455d502adc1f2cb2.png',
       category: "Food"),
