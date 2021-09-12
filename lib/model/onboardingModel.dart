bbf531c513b3b9b617b0e9f00a35d2845d44446b
Aniket762 <aniketindian8@gmail.com>
2021-09-12 16:27:41 +0530

    Enhacement: Onboarding 3d vectors added

--- lib/model/onboardingModel.dart
+++ lib/model/onboardingModel.dart
@@ -10,18 +10,18 @@ List<OnboardingModel> onboardingContent = [
   OnboardingModel(
       title: 'Hi I\'m Donna',
       image:
-          'https://cdn.dribbble.com/users/623359/screenshots/13670617/media/b020cb592153c999817dca341de2d782.png',
+          'https://res.cloudinary.com/dguy8qpzi/image/upload/v1631443916/Rectangle_39-removebg-preview_ptstqh.png',
       descp: "Transform your smart devices to intelligent communicators "),
   OnboardingModel(
       title: 'Take the first steps',
       image:
-          'https://cdn.dribbble.com/users/623359/screenshots/13670602/media/0f15bc59541b4176455d502adc1f2cb2.png',
+          'https://res.cloudinary.com/dguy8qpzi/image/upload/v1631444074/firstSteps-removebg-preview_abogdt.png',
       descp:
           "So many options for you to choose from. Make your life easier by just a tap "),
   OnboardingModel(
       title: 'Make it your own',
       image:
-          'https://cdn.dribbble.com/users/623359/screenshots/14149713/media/0f162f3f291fa31fc734534b408d73a8.png',
+          'https://res.cloudinary.com/dguy8qpzi/image/upload/v1631444144/make-your-own-removebg-preview_eefoag.png',
       descp:
           "Personalize Donna to fit your needs. Add your most used phrases and contact people you might need in distress."),
 ];
